c5cd00a0e9ffb7f07423949f3ec46bbd5b527b1d
bdemin <42113340+bdemin@users.noreply.github.com>
2019-10-19 18:26:04 +0300

    Create load_json.py

---
+++ load_json.py
@@ -0,0 +1,14 @@
+import json
+import numpy as np
+
+
+def load_json(path):
+    # Function to load a json file and convert it into np array
+    
+    with open(path) as f:
+        data = json.loads(f.read())
+        data = data['drawing']
+        data = [[x['x'], x['y']] for x in data]
+
+    skip = 100
+    return np.asarray(data)[0 : -1 : skip]
