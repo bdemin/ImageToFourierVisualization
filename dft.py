12a11edfea7887cfe536deece205541af1533885
bdemin <42113340+bdemin@users.noreply.github.com>
2019-10-28 01:40:01 +0200

    Update dft.py

--- dft.py
+++ dft.py
@@ -12,16 +12,17 @@ def dft(x):
         for n in range(N):
             phi = (2 * np.pi * k * n) / N
             c = complex(np.cos(phi), -np.sin(phi))
-            xn = complex(x[n][0],x[n][1])
+            xn = complex(x[n][0], x[n][1])
             _sum = _sum + (xn * c)
             
         _sum = complex(_sum.real / N, _sum.imag / N)
 
         freq = k
         amp = np.sqrt(_sum.real * _sum.real + _sum.imag * _sum.imag)
+
         phase = np.arctan2(_sum.imag, _sum.real)
         X.append({'freq': freq,
                 'amp': amp,
                 'phase': phase})
 
-    return sorted(X, key = lambda i: i['amp'], reverse = True) 
+    return sorted(X, key = lambda i: i['amp'], reverse = True)
