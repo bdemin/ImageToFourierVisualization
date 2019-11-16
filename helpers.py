871840dd3cdc66c7b14e4356e2270f3227320ae1
bdemin <42113340+bdemin@users.noreply.github.com>
2019-11-16 20:28:40 +0200

    Moved load_json to helpers file and removed load_json.py

--- helpers.py
+++ helpers.py
@@ -1,7 +1,20 @@
 import numpy as np
+import json
 
 from epicycle import Epicycle
 
+
+def load_json(path):
+    # Function to load a json file and convert it into np array
+    
+    with open(path) as f:
+        data = json.loads(f.read())
+        data = data['drawing']
+        data = [[x['x'], x['y']] for x in data]
+
+    skip = 10
+    return np.asarray(data)[0 : -1 : skip]
+    
 
 def build_data(N):
     # Optional function to build and return signal data for all epicycles
